26a35a51d2688488a97a4db6ebc4e67d2791133b
MatthewSharpOTU <matthew.sharp1@ontariotechu.net>
2022-07-06 14:54:17 -0400

    Added Hashing Class

---
+++ main.py
@@ -0,0 +1,22 @@
+import imp
+import importlib
+import os
+from playerStorage import HashTable
+
+'''
+t = HashTable()
+print(t.tbl)
+t["Matthew"] = 20
+t["Rayechelle"] = 21
+t["Isabella"] = 14
+t["Luz"] = 53
+t["Tim"] = 61
+t["JC"] = 21
+t["CJ"] = 30
+print(t["Matthew"])
+print(t.tbl)
+del t["JC"]
+print(t.tbl)
+del t["CJ"]
+print(t.tbl)
+'''
--- playerStorage.py
+++ playerStorage.py
@@ -1,1 +1,35 @@
-dict = {''}
+class HashTable:
+    def __init__(self):
+        self.MAX = 10
+        self.tbl = [[] for i in range(self.MAX)]
+
+    def get_hash(self, key):
+        hash = 0
+        for char in key:
+            hash += ord(char)
+        return hash%self.MAX
+    
+    def __setitem__(self, key, val):       
+        h = self.get_hash(key)
+        found = False
+        for i, element in enumerate(self.tbl[h]):
+            if len(element)==2 and element[0]==key:
+                self.tbl[h][i] = (key,val)
+                found = True
+                break
+        if not found:
+            self.tbl[h].append((key,val))
+        
+    def __getitem__(self, key):
+        h = self.get_hash(key)
+        for element in self.tbl[h]:
+            if element[0]==key:
+                return element[1]
+        return None
+    
+    def __delitem__(self,key):
+        h = self.get_hash(key)
+        for i, element in enumerate(self.tbl[h]):
+            if element[0]==key:
+                del self.tbl[h][i]
+
